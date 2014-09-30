021097d3a7ba4c6e6a2be6e4ef39ad8473ddc77e
Alex Woolford <awoolford@arrow.com>
2014-09-30 12:16:42 -0600

    Zookeeper and Kafka start automatically

---
+++ start-single-node-kafka.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./usr/local/kafka/kafka-0.8.1.1-src/bin/kafka-server-start.sh /usr/local/kafka/kafka-0.8.1.1-src/config/server.properties
---
+++ start-single-node-zookeeper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./usr/local/kafka/kafka-0.8.1.1-src/bin/zookeeper-server-start.sh /usr/local/kafka/kafka-0.8.1.1-src/config/zookeeper.properties
